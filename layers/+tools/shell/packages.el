2ac41a71f6708db055b6a119fb40db25933d9edf
Sylvain Benner <sylvain.benner@shopify.com>
2021-04-28 23:01:59 -0400

    [shell] Add support for shadowenv

--- layers/+tools/shell/packages.el
+++ layers/+tools/shell/packages.el
@@ -36,6 +36,7 @@
     multi-term
     org
     projectile
+    shadowenv
     (shell :location built-in)
     shell-pop
     (term :location built-in)
@@ -44,7 +45,6 @@
     vi-tilde-fringe
     window-purpose
     (vterm :toggle (not (spacemacs/system-is-mswindows)))))
-
 
 (defun shell/init-comint ()
   (setq comint-prompt-read-only t)
@@ -208,6 +208,9 @@
   (spacemacs/set-leader-keys
     "p'" #'spacemacs/projectile-shell-pop
     "p$" #'spacemacs/projectile-shell))
+
+(defun shell/pre-init-shadowenv ()
+  (add-to-list 'spacemacs--shadowenv-modes 'comint-mode))
 
 (defun shell/init-shell ()
   (spacemacs/register-repl 'shell 'shell)
